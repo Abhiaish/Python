a71f2bab3cca307d9238392288f89e48e944e8de
Abhinaya <abhinayaaish95@gmail.com>
2020-08-07 12:56:50 +0530

    Add files via upload

---
+++ code2.py
@@ -0,0 +1,15 @@
+# print all natural numbers from 1 to n.
+
+n=int(input())
+i=1
+while i<=n:
+    print(i)
+    i +=1
+
+# print all natural numbers from 1 to n.in reverse
+
+n=int(input())
+i=1
+while i<=n:
+    print(n)
+    n -=1
---
+++ code3.py
@@ -0,0 +1,9 @@
+# find sum of all natural numbers between 1 to n.
+
+n=int(input())
+
+sum=0
+for i in range(1,n):
+    print(sum)
+    sum +=i
+ 
