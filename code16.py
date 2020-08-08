a4de649c32ff8fadb3cd752f0590accf7f6db7e3
Abhinaya <abhinayaaish95@gmail.com>
2020-08-08 12:21:48 +0530

    Add files via upload

---
+++ code16.py
@@ -0,0 +1,11 @@
+# Calculate the Area of a Triangle
+
+a=float(input())
+b=float(input())
+c=float(input())
+
+s=(a+b+c)/2
+
+area= (s*(s-a)*(s-b)*(s-c)) ** 0.5
+
+print(area)
---
+++ code17.py
@@ -0,0 +1,4 @@
+#  to Generate a Random Number
+import random
+
+print(random.randint(0,9))
