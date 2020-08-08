daecd3798aee803c56c3136e12cebd72994b1560
Abhinaya <abhinayaaish95@gmail.com>
2020-08-08 12:22:13 +0530

    Add files via upload

---
+++ code18.py
@@ -0,0 +1,5 @@
+# Convert Kilometers to Miles
+km=float(input())
+ratio= 0.621371
+m=km*ratio
+print("The value in miles:",m)
---
+++ code19.py
@@ -0,0 +1,10 @@
+# Check if a Number is Positive, Negative or 0
+
+n=int(input("enter te number:"))
+
+if(n>0):
+    print("positive")
+elif n == 0:
+    print("zero")
+else:
+    print("negative")
